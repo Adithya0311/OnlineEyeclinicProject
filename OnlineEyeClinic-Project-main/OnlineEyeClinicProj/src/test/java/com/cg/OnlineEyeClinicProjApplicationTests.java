0ba68bebcb6ea839350818b7013440f146831301
Adithya0311 <lagishettyadithya@gmail.com>
2021-08-17 10:03:52 +0530

    SprintProject

---
+++ OnlineEyeClinic-Project-main/OnlineEyeClinicProj/src/test/java/com/cg/OnlineEyeClinicProjApplicationTests.java
@@ -0,0 +1,37 @@
+package com.cg;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.cg.dao.IPatientRepo;
+import com.cg.dao.ITestRepo;
+
+
+@SpringBootTest
+class OnlineEyeClinicProjApplicationTests {
+
+	@Autowired
+	 IPatientRepo repo;
+		@Test
+	public	void patientId() {
+			int id=1;
+			assertNotNull(repo.findById(id).get());
+		}
+		@Test
+		public void patientGet() {
+			assertNotNull(repo.findAll());
+		}
+		@Autowired
+		 ITestRepo repotest;
+			@Test
+		public	void testId() {
+				int id=1;
+				assertNotNull(repotest.findById(id).get());
+			}
+			@Test
+			public void testGet() {
+				assertNotNull(repotest.findAll());
+			}
+			
+}
+
